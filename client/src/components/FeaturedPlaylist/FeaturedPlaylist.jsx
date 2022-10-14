4a4ec27ec9e22dfe105a2c1315243e23d2d8883b
Patricia Jamriskova <patricia.jamriskova@sap.com>
2022-10-14 09:46:08 +0200

    add skeleton of sorting

--- client/src/components/FeaturedPlaylist/FeaturedPlaylist.jsx
+++ client/src/components/FeaturedPlaylist/FeaturedPlaylist.jsx
@@ -1,15 +1,62 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+import { useLazyQuery, useQuery } from '@apollo/client';
 import { PlaylistCard } from '../PlaylistCard/PlaylistCard';
 import { FEATURED_PLAYLIST } from '../../queries/featured-playlist';
 
 export const FeaturedPlaylist = () => {
-  const { loading, error, data } = useQuery(FEATURED_PLAYLIST);
+  //const { loading, error, data } = useQuery(FEATURED_PLAYLIST);
+  const [getPlaylists, {loading, error, data, called} ] = useLazyQuery(FEATURED_PLAYLIST);
+  const [sortedData, setSortedData] = useState(undefined)
+
+  console.log('LOADING: ',loading, 'ERROR: ',  error, 'DATA: ',  data, 'CALLED: ',called)
+  
+
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>{error.message}</div>;
+ // console.log(await getPlaylists(), 'xx_')
+
+ if (sortedData === undefined){
+  console.log('som v undefined')
+  getPlaylists()
+  if ( data) {
+    setSortedData(data?.featuredPlaylists.playlists.items.filter(i => i !== null))
+   }
+ } 
+console.log('DATA: ',data, 'SORTEDDATA', sortedData)
+
+
+  const sortPlaylistsName = (sortedRows) => {
+     return sortedRows.sort((a, b) => {
+      const nameA = a.name.toUpperCase()
+      const nameB = b.name.toUpperCase()
+
+      if (nameA<nameB) {
+        console.log('nameA<nameB')
+        return -1
+      }
+      if (nameA>nameB){
+        console.log('nameA>nameB')
+        return 1
+      }
+
+      return 0
+    }) 
+  }
+  const onClick = () => {
+const playlists = [...sortPlaylistsName(sortedData)]
+    setSortedData( playlists) 
+
+    
+  }
+
 
   return (
     <div>
       <h1>Playlists</h1>
-      {data?.featuredPlaylists.playlists.items.filter(i => i !== null).map((playlist, index) => (
+      <button onClick={onClick}>SORT</button>
+      <button onClick={onClick}>SORT_</button>
+      <div>{new Date().getTime()}</div>
+      {sortedData?.map((playlist, index) => (
         <div key={index}>
           <PlaylistCard
             keyId={playlist.id}
@@ -19,6 +66,7 @@ export const FeaturedPlaylist = () => {
             externalUrl={playlist.external_urls.spotify}
           />
         </div>
+        
       ))}
     </div>
   );
