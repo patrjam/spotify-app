24e70aca07fcb32b09788373c15cd1c5b37db530
Patrícia Jamrišková <patricia.jamriskova@sap.com>
2022-10-14 17:34:49 +0200

    (feat): sort playlists (#2)
    * add sorting to /playlists page

--- client/src/components/FeaturedPlaylist/FeaturedPlaylist.jsx
+++ client/src/components/FeaturedPlaylist/FeaturedPlaylist.jsx
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { PlaylistCard } from '../PlaylistCard/PlaylistCard';
 import { FEATURED_PLAYLIST } from '../../queries/featured-playlist';
+import { sortName } from '../../customFunctions/sortName';
+import Asc from '../../assets/asc.svg';
+import Desc from '../../assets/desc.svg';
+import Sort from '../../assets/sort.svg';
+import './styles.css';
 
 export const FeaturedPlaylist = () => {
   const { loading, error, data } = useQuery(FEATURED_PLAYLIST);
+  const [sortedData, setSortedData] = useState(undefined);
+  const [orderBy, setOrderBy] = useState(0);
+
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>{error.message}</div>;
+
+  const playlists = data?.featuredPlaylists.playlists.items.filter(
+    (i) => i !== null
+  );
+
+  if (sortedData) {
+    if (orderBy == 1) {
+      sortName(playlists, false);
+    }
+    if (orderBy == 2) {
+      sortName(playlists, true);
+    }
+  }
+
+  const onClick = () => {
+    setSortedData(true);
+    setOrderBy(1);
+
+    if (orderBy === 1) {
+      return setOrderBy(2);
+    }
+    if (orderBy === 2) {
+      return setOrderBy(0);
+    }
+  };
+
+  let button;
+  
+  if (orderBy === 1) {
+    button = Asc;
+  } else if (orderBy === 2) {
+    button = Desc;
+  } else button = Sort;
 
   return (
     <div>
       <h1>Playlists</h1>
-      {data?.featuredPlaylists.playlists.items.filter(i => i !== null).map((playlist, index) => (
+      <button
+        className={'sort-btn'}
+        onClick={onClick}
+        style={{ backgroundImage: `url(${button})` }}
+      >
+      </button>
+      {playlists?.map((playlist, index) => (
         <div key={index}>
           <PlaylistCard
             keyId={playlist.id}
